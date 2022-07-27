76073b589168868b94e79b73a263688a3f9b665d
maumans <58917708+maumans@users.noreply.github.com>
2022-07-27 15:32:20 +0000

    api ok

---
+++ database/seeders/ModePaiementSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Mode_paiement;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ModePaiementSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('etablissements')->delete();
+
+        Mode_paiement::create(["libelle","OM"]);
+        Mode_paiement::create(["libelle","CASH"]);
+    }
+}
