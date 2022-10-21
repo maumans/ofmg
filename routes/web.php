1e6cf029683cf2a148b2a276559e267a224c56da
maumans <58917708+maumans@users.noreply.github.com>
2022-10-21 16:45:36 +0000

    update route method

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -98,7 +99,7 @@ Route::resource('paiement',\App\Http\Controllers\PaiementController::class);
 Route::get("paiement/search/{code?}/{matricule?}",[\App\Http\Controllers\PaiementController::class,"search"])->name("paiement.search");
 Route::get("paiement/ok/{apprenantId}/{total}",[\App\Http\Controllers\PaiementController::class,"ok"])->name("paiement.ok");
 
-Route::get('api/orange/notifications', function (){
+Route::post('api/orange/notifications', function (Request $request){
     return "Orange Callback";
 });
 
