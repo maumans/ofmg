762d09080d6fcedf6463df374690beb09337e9b3
Amadou Bailo Diallo <amadou-bailo.diallo@addvalis.com>
2022-10-24 16:47:33 +0000

    api.php édité en ligne via Bitbucket

--- routes/api.php
+++ routes/api.php
@@ -29,11 +29,16 @@ Route::middleware('auth:api')->group(function () {
     Route::post("etablissement/apprenant/paiement/",[\App\Http\Controllers\Api\EtablissementController::class,"paiement"])->name("api.etablissement.apprenant.paiement");
 });
 
+Route::middleware(['auth.basic'])->any('orange/notifications', function (Request $request){
+    return "Orange Callback";
+});
+
 
 
 
 Route::post('login',[App\Http\Controllers\Api\AuthController::class,"login"] );
 Route::post('register',[App\Http\Controllers\Api\AuthController::class,"register"] );
+
 
 
 //Route::apiResource("etablissement",App\Http\Controllers\Api\EtablissementController::class)->only("show");
