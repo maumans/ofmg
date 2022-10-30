35931a06e3f1114dbf24a9bb5c48369f04a243f8
Amadou Diallo <amadou-bailo.diallo@addvalis.com>
2022-10-30 14:41:53 +0000

    Correction OmB2B

--- package/crudfy/om-b2b/src/Commands/OmB2bCommand.php
+++ package/crudfy/om-b2b/src/Commands/OmB2bCommand.php
@@ -5,7 +5,7 @@ namespace Crudfy\OmB2b\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Artisan;
 
-class OmB2BCommand extends Command
+class OmB2bCommand extends Command
 {
     /**
      * The name and signature of the console command.
