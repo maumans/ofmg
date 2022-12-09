0750d63e2c94f98f8a281d4bb945bfcc7a44dd9a
maumans <58917708+maumans@users.noreply.github.com>
2022-12-09 16:03:35 +0000

    migrations

--- database/migrations/2022_09_20_160742_create_paiement_occasionnels_table.php
+++ database/migrations/2022_09_20_160742_create_paiement_occasionnels_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePaiementOccasionelsTable extends Migration
+class CreatePaiementOccasionnelsTable extends Migration
 {
     /**
      * Run the migrations.
