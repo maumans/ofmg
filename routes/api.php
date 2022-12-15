58c94d639f94c77c7ff8a8ea932e38d45916bccb
maumans <58917708+maumans@users.noreply.github.com>
2022-12-15 11:04:01 +0000

    callback

--- routes/api.php
+++ routes/api.php
@@ -60,105 +60,124 @@ Route::middleware("auth.basic")->post('orange/notifications', function (Request 
 
         $transaction->save();
 
-        $paiementGlobal = PaiementGlobal::where("id",$transaction->item_key)->first();
-
-        $paiementGlobal->transaction()->associate($transaction)->save();
-
-        if($transaction->type == "CASHOUT")
-        {
-            if($transaction->status=="SUCCESS")
-            {
-
-                foreach ($paiementGlobal->paiements as $paiement)
-                {
-                    $resteApayer=$paiement->tarif["montant"]-$paiement->apprenant->paiements->where("type_paiement_id",$paiement["type_paiement_id"])->sum("montant");
-                    $paiement->apprenant->tarifs()->syncWithoutDetaching([$paiement->tarif->id=>["resteApayer"=>$resteApayer]]);
-
-
-                    foreach($paiement->apprenant->tarifs as $tarif)
-                    {
-                        $payeParTarif=$paiement->apprenant->paiements->where("tarif_id",$tarif->id)->sum("montant");
-
-                        $intervalle=CarbonPeriod::create($tarif->anneeScolaire->dateDebut,"1 month",$tarif->anneeScolaire->dateFin)->roundMonth();
-
-                        $nombreMois=$intervalle->count();
-
-                        $sommeMensuelle=$tarif->montant/$nombreMois;
-
-                        $repartition=$payeParTarif;
-
-                        //dd($payeParTarif,$sommeMensuelle,$tarif->montant);
-
-
-                        foreach($intervalle as $date)
-                        {
-
-
-                            $moisId=Mois::where("position",$date->month)->first()->id;
-
-
-
-                            $moisPaye=Mois_Paye::where("apprenant_tarif_id",$tarif->pivot->id)->where("mois_id",$moisId)->first();
-
-
-                            if($repartition>=$sommeMensuelle)
-                            {
-                                $moisPaye->montant=$sommeMensuelle;
-                                $moisPaye->save();
-                                $repartition=$repartition-$sommeMensuelle;
-
-                            }
-                            else
-                            {
-                                if($repartition==0)
-                                {
-                                    $moisPaye->montant=0;
-                                    $moisPaye->save();
-
-
-                                }
-                                else
-                                {
-                                    $moisPaye->montant=$repartition;
-                                    $moisPaye->save();
-                                    $repartition=0;
-                                }
-                            }
-
-                        }
-
-                    }
-                }
-            }
-        }
 
         if($transaction->item_model === "App\Models\Paiement")
         {
-            $paiement=Paiement::where("transaction_id",$transaction->id)->first();
+
+            $paiement = Paiement::where("id",$transaction->item_key)->first();
+
+            $paiement->transaction()->associate($transaction)->save();
+
             $paiement->transaction_status=$transaction->status;
+
             $paiement->save();
         }
         else if($transaction->item_model === "App\Models\PaiementGlobal")
         {
-            $paiementGlobal=PaiementGlobal::where("transaction_id",$transaction->id)->first();
+            $paiementGlobal = PaiementGlobal::where("id",$transaction->item_key)->first();
+
+            $paiementGlobal->transaction()->associate($transaction)->save();
+
             $paiementGlobal->transaction_status=$transaction->status;
+
             $paiementGlobal->save();
+
+            if($transaction->type == "CASHOUT")
+            {
+
+                if($transaction->status=="SUCCESS")
+                {
+
+                    foreach ($paiementGlobal->paiements as $paiement)
+                    {
+                        $resteApayer=$paiement->tarif["montant"]-$paiement->apprenant->paiements->where("type_paiement_id",$paiement["type_paiement_id"])->sum("montant");
+                        $paiement->apprenant->tarifs()->syncWithoutDetaching([$paiement->tarif->id=>["resteApayer"=>$resteApayer]]);
+
+
+                        foreach($paiement->apprenant->tarifs as $tarif)
+                        {
+                            $payeParTarif=$paiement->apprenant->paiements->where("tarif_id",$tarif->id)->sum("montant");
+
+                            $intervalle=CarbonPeriod::create($tarif->anneeScolaire->dateDebut,"1 month",$tarif->anneeScolaire->dateFin)->roundMonth();
+
+                            $nombreMois=$intervalle->count();
+
+                            $sommeMensuelle=$tarif->montant/$nombreMois;
+
+                            $repartition=$payeParTarif;
+
+                            //dd($payeParTarif,$sommeMensuelle,$tarif->montant);
+
+
+                            foreach($intervalle as $date)
+                            {
+
+
+                                $moisId=Mois::where("position",$date->month)->first()->id;
+
+
+
+                                $moisPaye=Mois_Paye::where("apprenant_tarif_id",$tarif->pivot->id)->where("mois_id",$moisId)->first();
+
+
+                                if($repartition>=$sommeMensuelle)
+                                {
+                                    $moisPaye->montant=$sommeMensuelle;
+                                    $moisPaye->save();
+                                    $repartition=$repartition-$sommeMensuelle;
+
+                                }
+                                else
+                                {
+                                    if($repartition==0)
+                                    {
+                                        $moisPaye->montant=0;
+                                        $moisPaye->save();
+
+
+                                    }
+                                    else
+                                    {
+                                        $moisPaye->montant=$repartition;
+                                        $moisPaye->save();
+                                        $repartition=0;
+                                    }
+                                }
+
+                            }
+
+                        }
+                    }
+                }
+            }
         }
         else if($transaction->item_model === "App\Models\Salaire")
         {
-            $salaire=Salaire::where("transaction_id",$transaction->id)->first();
+            $salaire = Salaire::where("id",$transaction->item_key)->first();
+            $salaire->transaction()->associate($transaction)->save();
             $salaire->transaction_status=$transaction->status;
-            $salaire->niveauValidation=2;
-            $salaire->status="VALIDE";
+
+            if($transaction->status =="SUCCESS")
+            {
+                $salaire->niveauValidation=2;
+                $salaire->status="VALIDE";
+            }
+
             $salaire->save();
         }
         else if($transaction->item_model === "App\Models\Paiement_occasionnel")
         {
-            $paiementOccasionnel=Paiement_occasionnel::where("transaction_id",$transaction->id)->first();
+            $paiementOccasionnel = Paiement_occasionnel::where("id",$transaction->item_key)->first();
+
+            $paiementOccasionnel->transaction()->associate($transaction)->save();
+
             $paiementOccasionnel->transaction_status=$transaction->status;
 
-            $paiementOccasionnel->niveauValidation=2;
-            $paiementOccasionnel->status="VALIDE";
+            if($transaction->status =="SUCCESS")
+            {
+                $paiementOccasionnel->niveauValidation=2;
+                $paiementOccasionnel->status="VALIDE";
+            }
 
             $paiementOccasionnel->save();
         }
